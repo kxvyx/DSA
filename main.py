4a7f0bd9e4ef102e179c4d8d7810422d13e0fa1f
Kavya <kavyapramodpatil007@gmail.com>
2026-01-11 12:21:55 +0530

    refined isFull, add and delete

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
-class StaticList():
-    def __init__(self,length):
+class StaticList:
+    #assumed same data type for each element in list
+    def __init__(self,length):#O(N)
         self.length = length
         self.list = [None]*length
+        self.current = 0
 
-    def isFull(self):
-        if None in self.list:
+    def isFull(self):#O(1)
+        if self.current<self.length:
             return False
+        else:
+            return True
         
-    def add(self,index,val):
-        if not self.isFull() and index<self.length:
-            self.list[index] = val
-
-    def delete(self,index):
-        if index<self.length:
+    def add(self,index,val):#O(1)
+        try:
+            if val is None:
+                raise Exception
+            if not isinstance(index,int):
+                raise Exception
+            if not self.isFull() and 0<=index<self.length:
+                if self.list[index] is None:
+                    self.list[index] = val
+                    self.current+=1
+                else:
+                    self.list[index] = val
+        except Exception as e:
+            print("None value cannot be added")
+        
+           
+#time complexity make 
+    def delete(self,index):#O(1)
+        if 0<=index<self.length and self.list[index] is not None:
             self.list[index] = None
+            self.current-=1
             
 
 
