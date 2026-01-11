c8a058e3638a68d2542fa31a611cf17864d76134
Kavya <kavyapramodpatil007@gmail.com>
2026-01-11 20:52:50 +0530

    made required changes + completed error handling

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ class StaticList:
     def __init__(self,length):#O(N)
         self.length = length
         self.list = [None]*length
-        self.current = 0
+        self.current_count = 0
 
     def isFull(self):#O(1)
-        if self.current<self.length:
+        if self.current_count<self.length:
             return False
         else:
             return True
@@ -14,24 +14,31 @@ class StaticList:
     def add(self,index,val):#O(1)
         try:
             if val is None:
-                raise Exception
+                raise ValueError("Cannot add None to list")
             if not isinstance(index,int):
-                raise Exception
+                raise TypeError("Index should be integer only")
             if not self.isFull() and 0<=index<self.length:
                 if self.list[index] is None:
-                    self.list[index] = val
-                    self.current+=1
-                else:
-                    self.list[index] = val
-        except Exception as e:
-            print("None value cannot be added")
+                    self.current_count+=1
+                self.list[index] = val
+        except ValueError as e:
+            print(e)
+        except TypeError as e:
+            print(e)
+        except Exception:
+            print("Something went wrong :/")
         
-           
-#time complexity make 
     def delete(self,index):#O(1)
-        if 0<=index<self.length and self.list[index] is not None:
-            self.list[index] = None
-            self.current-=1
+        try:
+            if not isinstance(index,int):
+                raise TypeError("Index should be integer only")
+            if 0<=index<self.length and self.list[index] is not None:
+                self.list[index] = None
+                self.current_count-=1
+        except TypeError as e:
+            print(e)
+        except Exception:
+            print("Something went wrong :/")
             
 
 
