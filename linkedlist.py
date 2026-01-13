9cb87e0aef5b61c8b57069a1e2aea1362673efd9
Kavya <kavyapramodpatil007@gmail.com>
2026-01-13 15:53:20 +0530

    add() and insert() implemented

--- linkedlist.py
+++ linkedlist.py
@@ -11,6 +11,24 @@ class LinkedList:
 
     def traverse(self):
         current = self.head
-        while current is not None:
+        while current:
             print(current.val)
             current = current.next
+    
+    def add(self,node): #only add node at end of LL
+        cur = self.head
+        while cur.next:
+            cur = cur.next
+        cur.next = node
+    
+    def insert(self,position,node): #what if position more than length ?
+        if position== 1:
+            node.next = self.head
+            self.head = node
+        else:
+            cur = self.head
+            for i in range(position-2):
+                cur = cur.next
+            next_node = cur.next
+            cur.next = node
+            node.next = next_node
