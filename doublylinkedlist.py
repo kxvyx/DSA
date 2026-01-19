298f406f7edd3a82daf99e0d1d7d22b755e6fe6e
Kavya <kavyapramodpatil007@gmail.com>
2026-01-19 15:03:18 +0530

    full DLL code

---
+++ doublylinkedlist.py
@@ -0,0 +1,115 @@
+class DoublyNode:
+    def __init__(self,val,prev=None,next=None):
+        self.val = val
+        self.prev = prev
+        self.next = next
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.size = 0
+        dummy_node = DoublyNode(None)
+        self.head = dummy_node
+        self.tail = dummy_node
+
+    def traverse(self):#O(N)
+        cur = self.head.next
+        while cur:
+            print(cur.val,end=" -> ")
+            cur = cur.next
+    
+    def reverseTraverse(self):#O(N)
+        cur = self.tail
+        while cur!=self.head:
+            print(cur.val,end=" -> ")
+            cur = cur.prev
+
+    def insert_at_end(self,node):#O(1)
+        self.tail.next = node
+        node.prev = self.tail
+        self.tail = node
+        self.size+=1
+
+    def delete_last_node(self):#O(1)
+        if self.size>0:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            self.size-=1
+        else:
+            print("list is empty")
+
+    def insert_at_position(self,position,new_node):
+        try:
+            if position<=0 or position>self.size+1:
+                raise ValueError("position not in range")
+            elif position==self.size+1:
+                self.insert_at_end(new_node)
+            else:
+                cur = self.head
+                for i in range(position-1):
+                    cur = cur.next
+                new_node.next = cur.next
+                new_node.prev = cur
+                cur.next.prev = new_node
+                cur.next = new_node
+                self.size += 1
+        except ValueError as e:
+            print(e)
+        except Exception as e:
+            print("something went wrong", e)
+
+    def delete_at_position(self,position):
+        try:
+            if position<=0 or position>self.size:
+                raise ValueError("position not in range")
+            elif position==self.size:
+                self.delete_last_node()
+            else:
+                cur = self.head
+                for i in range(position):
+                    cur = cur.next
+                cur.next.prev = cur.prev
+                cur.prev.next = cur.next
+                self.size -= 1
+        except ValueError as e:
+            print(e)
+        except Exception as e:
+            print("something went wrong", e)
+
+    def modify_val_at_position(self,position,new_val):
+        try:
+            if self.size==0:
+                print("list is empty")
+                return 
+            if position<=0 or position>self.size:
+                raise ValueError("position not in range") 
+            cur = self.head
+            for i in range(position):
+                cur = cur.next
+            cur.val = new_val
+        except ValueError as e:
+            print(e)
+        except Exception as e:
+            print("something went wrong", e)
+
+if __name__=="__main__":
+    DLL = DoublyLinkedList()
+    # DLL.insert_at_end(DoublyNode(1))
+    # DLL.insert_at_end(DoublyNode(2))
+    # DLL.insert_at_end(DoublyNode(3))
+    print(DLL.traverse()) #1 -> 2 -> 3 -> None
+    print("size of DLL: ",DLL.size)
+    #print(DLL.reverseTraverse())
+    # DLL.delete_last_node() #1 -> 2 -> None
+    # print(DLL.traverse())
+    # print("size of DLL: ",DLL.size)
+    DLL.insert_at_position(DLL.size,DoublyNode(4))
+    print(DLL.traverse())
+    print("size of DLL: ",DLL.size)
+
+    DLL.delete_at_position(4)
+    print(DLL.traverse())
+    print("size of DLL: ",DLL.size)
+
+    DLL.modify_val_at_position(2,20)
+    print(DLL.traverse())
+
